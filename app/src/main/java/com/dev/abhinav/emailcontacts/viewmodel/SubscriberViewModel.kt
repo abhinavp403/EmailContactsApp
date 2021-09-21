795307c4db0270b18bbc90c9657d89a3997e981b
Abhinav <abhinavp403@gmail.com>
2021-09-20 22:15:20 -0500

    First Commit

--- app/src/main/java/com/dev/abhinav/emailcontacts/viewmodel/SubscriberViewModel.kt
+++ app/src/main/java/com/dev/abhinav/emailcontacts/viewmodel/SubscriberViewModel.kt
@@ -1,4 +1,32 @@
 package com.dev.abhinav.emailcontacts.viewmodel
 
-class SubscriberViewModel {
+import android.provider.SyncStateContract.Helpers.insert
+import androidx.databinding.Bindable
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.dev.abhinav.emailcontacts.database.Subscriber
+import com.dev.abhinav.emailcontacts.repository.SubscriberRepository
+
+class SubscriberViewModel(private val repository: SubscriberRepository) : ViewModel() {
+
+    @Bindable val inputName = MutableLiveData<String>()
+    @Bindable val inputEmail = MutableLiveData<String>()
+    @Bindable val saveOrUpdateButtonText = MutableLiveData<String>()
+    @Bindable val clearAllOrDeleteButtonText = MutableLiveData<String>()
+
+    init {
+        saveOrUpdateButtonText.value = "Save"
+        clearAllOrDeleteButtonText.value = "Clear All"
+    }
+
+    fun saveOrUpdate() {
+        //insertSubscriber(Subscriber(0, inputName.value!!, inputEmail.value!!))
+        inputName.value = ""
+        inputEmail.value = ""
+    }
+
+    fun clearAllOrDelete() {
+
+    }
+
 }
